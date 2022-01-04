91dbad8111363abd18080b2b751a627e1f5823d5
Zarthan <46505448+Zarthan66@users.noreply.github.com>
2022-01-04 14:38:51 +0700

    Initial Release

---
+++ Sistem Pakar/Diseases.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistem_Pakar
+{
+    internal class Diseases
+    {
+        protected string name;
+        protected int stats;
+        List<Symptoms> symptoms = new List<Symptoms>();
+
+        public Diseases(string name)
+        {
+            this.name = name;
+            
+        }
+
+        /*
+        public void emptyDiseaseClass()
+        {
+            symptopms.Clear();
+            this.name = "";
+        }*/
+
+        public string getDiseaseName()
+        {
+            return this.name;
+        }
+
+        public int getStats()
+        {
+            return this.stats;
+        }
+
+        public string getSymptomName(int index)
+        {
+            return symptoms[index].getSymptomName();
+        }
+
+        public int getSymptomStats(int index)
+        {
+            return symptoms[index].getSymptomStats();
+        }
+
+        public void setDiseaseStats(int value)
+        {
+            this.stats = value;
+        }
+
+        public void setSymptomStats(int index, int value)
+        {
+            symptoms[index].setSymptomStats(value);
+        }
+
+        public void addSymptom(string name)
+        {
+            Symptoms newSymptom = new Symptoms(name);
+            symptoms.Add(newSymptom);
+        }
+
+        public int getSymptomSize()
+        {
+            return symptoms.Count;
+        }
+    }
+}
